b13f04ebfcc140b429d5149c03860ee2e056a7b9
hardik-nividata <hardikp@nividata.com>
2026-01-18 19:30:22 +0530

    introduce fav model and apis

--- backend/super_hero/serializers.py
+++ backend/super_hero/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import SuperHero
+from .models import SuperHero, FavoriteHero
 
 
 class SuperHeroListSerializer(serializers.ModelSerializer):
@@ -9,7 +9,42 @@ class SuperHeroListSerializer(serializers.ModelSerializer):
                   "image_url", "intelligence", "strength", "speed",
                   "durability", "power", "combat")
 
+
 class SuperHeroDetailSerializer(serializers.ModelSerializer):
+    is_favorite = serializers.SerializerMethodField()
+
     class Meta:
         model = SuperHero
         fields = "__all__"
+
+    def get_is_favorite(self, obj):
+        request = self.context.get("request")
+
+        if not request or not request.user.is_authenticated:
+            return None
+
+        return FavoriteHero.objects.filter(
+            user=request.user,
+            hero=obj
+        ).exists()
+
+
+class UserFavSuperHeroListSerializer(serializers.ModelSerializer):
+    is_favorite = serializers.SerializerMethodField()
+
+    class Meta:
+        model = SuperHero
+        fields = ("id", "name", "slug", "alignment", "publisher",
+                  "image_url", "intelligence", "strength", "speed",
+                  "durability", "power", "combat", "is_favorite")
+
+    def get_is_favorite(self, obj):
+        request = self.context.get("request")
+
+        if not request or not request.user.is_authenticated:
+            return None
+
+        return FavoriteHero.objects.filter(
+            user=request.user,
+            hero=obj
+        ).exists()
--- backend/super_hero/views.py
+++ backend/super_hero/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from .serializers import SuperHeroListSerializer, SuperHeroDetailSerializer
-from .models import SuperHero
-from rest_framework.pagination import PageNumberPagination
+from .serializers import SuperHeroListSerializer, SuperHeroDetailSerializer, UserFavSuperHeroListSerializer
+from .models import SuperHero, FavoriteHero
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -16,3 +19,54 @@ class SuperHeroDetailApiView(RetrieveAPIView):
     serializer_class = SuperHeroDetailSerializer
     queryset = SuperHero.objects.all()
     lookup_field = "slug"
+
+
+class UserFavSuperHerosListApiView(ListAPIView):
+    serializer_class = UserFavSuperHeroListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return (
+            SuperHero.objects
+            .filter(favorited_by__user=self.request.user)
+            .distinct().order_by("id")
+        )
+
+
+class FavSuperHeroApiView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, hero_id):
+        is_favorite = FavoriteHero.objects.filter(
+            user=request.user,
+            hero_id=hero_id,
+        ).exists()
+
+        return Response(
+            {"is_favorite": is_favorite},
+            status=status.HTTP_200_OK,
+        )
+
+    def post(self, request, hero_id):
+        hero = get_object_or_404(SuperHero, id=hero_id)
+
+        fav, created = FavoriteHero.objects.get_or_create(
+            user=request.user,
+            hero=hero,
+        )
+
+        return Response(
+            {"is_favorite": True},
+            status=status.HTTP_201_CREATED if created else status.HTTP_200_OK,
+        )
+
+    def delete(self, request, hero_id):
+        FavoriteHero.objects.filter(
+            user=request.user,
+            hero_id=hero_id,
+        ).delete()
+
+        return Response(
+            {"is_favorite": False},
+            status=status.HTTP_204_NO_CONTENT,
+        )
